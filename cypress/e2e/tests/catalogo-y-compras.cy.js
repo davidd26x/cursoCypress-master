7992e27311f3fc70195a54f919bc6b501ba434a5
davidd26x <jose01265@gmail.com>
2025-05-14 10:34:58 -0600

    Navegación por categorías

--- cypress/e2e/tests/catalogo-y-compras.cy.js
+++ cypress/e2e/tests/catalogo-y-compras.cy.js
@@ -42,48 +42,48 @@ describe(CommonPageData.testSuites.catalogoYCompras, () => {
     })
 
 
-    it('Agregar producto al carrito', () => {
+    // it('Agregar producto al carrito', () => {
 
-        //Paso 1
-        Logger.stepNumber(1)
-        Logger.step('Iniciar sesión como usuario registrado'); 
-        Logger.subStep('Navegar a la página de Demoblaze.com');  //Subpaso 1
-        CommonPageMethods.navigateToDemoBlaze();
-        Logger.subStep('Click on Log in link')  //Subpaso 1.1
-        CommonPageMethods.clickOnLogInOption();
-        LoginMethods.loginGlobal(user.username, user.password)
+    //     //Paso 1
+    //     Logger.stepNumber(1)
+    //     Logger.step('Iniciar sesión como usuario registrado'); 
+    //     Logger.subStep('Navegar a la página de Demoblaze.com');  //Subpaso 1
+    //     CommonPageMethods.navigateToDemoBlaze();
+    //     Logger.subStep('Click on Log in link')  //Subpaso 1.1
+    //     CommonPageMethods.clickOnLogInOption();
+    //     LoginMethods.loginGlobal(user.username, user.password)
         
-        //Paso 2
-        Logger.stepNumber(2)
-        Logger.step('Navegar a la página de inicio') 
-        CommonPageMethods.clickOnHomeOption();
+    //     //Paso 2
+    //     Logger.stepNumber(2)
+    //     Logger.step('Navegar a la página de inicio') 
+    //     CommonPageMethods.clickOnHomeOption();
 
-        //Paso 3
-        Logger.stepNumber(3)
-        Logger.step('Seleccionar una categoría de productos en el menú de navegación')
-        HomeMethods.clickOnMonitors()
+    //     //Paso 3
+    //     Logger.stepNumber(3)
+    //     Logger.step('Seleccionar una categoría de productos en el menú de navegación')
+    //     HomeMethods.clickOnMonitors()
 
-        //Paso 4
-        Logger.stepNumber(4)
-        Logger.step('Hacer clic en un producto específico')
-        HomeMethods.clickOnProductLink(product)
+    //     //Paso 4
+    //     Logger.stepNumber(4)
+    //     Logger.step('Hacer clic en un producto específico')
+    //     HomeMethods.clickOnProductLink(product)
 
-        //Paso 5
-        Logger.stepNumber(5)
-        Logger.verification('Verificar que se muestra la página de detalles del producto')
-        ProductDetailsMethods.verifyProductDetailsPageIsDisplayed();
+    //     //Paso 5
+    //     Logger.stepNumber(5)
+    //     Logger.verification('Verificar que se muestra la página de detalles del producto')
+    //     ProductDetailsMethods.verifyProductDetailsPageIsDisplayed();
 
-        //Paso 6
-        Logger.stepNumber(6)
-        Logger.step('Hacer clic en el botón Add to cart')
-        ProductDetailsMethods.clickOnAddToCartButton();
+    //     //Paso 6
+    //     Logger.stepNumber(6)
+    //     Logger.step('Hacer clic en el botón Add to cart')
+    //     ProductDetailsMethods.clickOnAddToCartButton();
 
-        //Paso 7
-        Logger.stepNumber(7)
-        Logger.verification('Verificar que se muestre mensaje de verificación')
-        ProductDetailsMethods.verifyProductAddedMessage();
-        CommonPageMethods.clickOnCartOption();
-        CartMethods.verifyProductAdded(product);
+    //     //Paso 7
+    //     Logger.stepNumber(7)
+    //     Logger.verification('Verificar que se muestre mensaje de verificación')
+    //     ProductDetailsMethods.verifyProductAddedMessage();
+    //     CommonPageMethods.clickOnCartOption();
+    //     CartMethods.verifyProductAdded(product);
 
-    })
+    // })
 })
